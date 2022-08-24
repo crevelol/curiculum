21a133739d7ce9f1bbf2b4aa4bcbc7536e30a45e
Carlos Revelo <carlosrevla@hotmail.com>
2022-08-24 13:07:22 -0500

    first commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,19 @@
+/**
+* Template Name: BizLand - v3.8.1
+* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+  
+})()
